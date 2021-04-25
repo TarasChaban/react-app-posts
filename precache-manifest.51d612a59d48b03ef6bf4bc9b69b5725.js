3a25d051242526b49e4690535e74c9ada90b97b5
Taras Chaban <tarekchaban@gmail.com>
2021-04-25 20:52:20 +0300

    Updates

---
+++ precache-manifest.51d612a59d48b03ef6bf4bc9b69b5725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4cf0785a2916c48b8f6a6562f237d73",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c9bb23ac1772d94986ca",
+    "url": "./static/css/main.d07f4d78.chunk.css"
+  },
+  {
+    "revision": "05bc24f241802333d844",
+    "url": "./static/js/2.8f24e77a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.8f24e77a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9bb23ac1772d94986ca",
+    "url": "./static/js/main.ae705ac6.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
